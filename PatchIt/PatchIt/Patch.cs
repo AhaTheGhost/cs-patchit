1e0d8d4325f75fe24251a595cbd2dd3960472bb9
AhmedFShark <ahmad360pro@gmail.com>
2023-09-13 21:30:40 +0200

    1.1.0.0 Update

--- PatchIt/PatchIt/Patch.cs
+++ PatchIt/PatchIt/Patch.cs
@@ -1,121 +1,137 @@
-/**
- * @author Ahmed F. Shark <ahmad360pro@gmail.com>
- * @version 1.0.4
- * @link https://github.com/AhaTheGhost/cs-patchit/
- */
-
 using System;
 using System.Runtime.CompilerServices;
 
+
 namespace PatchIt
 {
-    public class Patch : IDisposable
-    {
+    /**
+     * @author Ahmed F.Shark<ahmad360pro@gmail.com>
+     * @version 1.1.0.0
+     * @link https://github.com/AhaTheGhost/cs-patchit/
+     */
 
-        private byte[] FindBinary;
-        private byte[] ReplaceBinary;
-        private byte[] ByteArray;
+    /// <summary>
+    /// A class for patching binary data.
+    /// </summary>
+
+    public class BinaryPatcher : IDisposable
+    {
+        // The array to be patched.
+        private byte[] byteArray;
         public string LastLog;
 
+        // Constant representing the library name.
+        const string LIB_NAME = "PatchIt";
+
+        // Constructor for initializing the BinaryPatcher.
         [MethodImpl(MethodImplOptions.NoInlining)]
-
-        public Patch(byte[] byteArray)
+        public BinaryPatcher(byte[] byteArray)
         {
-            ByteArray = byteArray;
+            // Check if the input byte array is null and throw an exception if it is.
+            if (byteArray == null)
+                throw new ArgumentNullException(nameof(byteArray), $"{LIB_NAME}: Input byte array cannot be null.");
+
+            this.byteArray = byteArray;
+            LastLog = "Initialized";
         }
 
-        public void patchData(byte[][] array)
+        // Method to apply binary patches to the byte array.
+        public void PatchData(byte[][] patchPairs)
         {
-
-            if (isArraySizeVaild(array))
-                buffer(array);
-
-        }
-
-        private void buffer(byte[][] array)
-        {
+            IsPatchPairsSizeValid(patchPairs);
 
             try
             {
-
-                for (int i = 0; i < array.Length; i += 2)
-                {
-
-                    FindBinary = array[i];
-                    ReplaceBinary = array[i + 1];
-
-                    for (int F = 0; F <= ByteArray.Length - 1; F++)
-                    {
-                        if (!isSequenceVaild(ByteArray, F))
-                            continue;
-                        else
-                        {
-                            for (int R = 0; R <= FindBinary.Length - 1; R++)
-                                ByteArray[F + R] = ReplaceBinary[R];
-                            break;
-                        }
-                    }
-
-                }
-
-                LastLog = "success";
-
+                ApplyPatches(patchPairs);
+                LastLog = "Success";
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new InvalidOperationException($"{LIB_NAME} Error: Index out of range - {ex.Message}", ex);
             }
             catch (Exception e)
             {
-                LastLog = "error: " + e;
+                throw new InvalidOperationException($"{LIB_NAME} Error: {e.Message}", e);
             }
-
         }
 
-        private bool isArraySizeVaild(byte[][] array)
+        // Method to apply the patches to the byte array.
+        private void ApplyPatches(byte[][] patchPairs)
         {
-
-            if (array.Length % 2 != 0)
+            try
             {
-                LastLog = "The array must contain as many \"find\" sub arraies as \"replace\" arraies. Size of Find and Replace Array: " + array.Length;
-                return false;
+                // Iterate through the patchPairs and apply each patch.
+                for (int i = 0; i < patchPairs.Length; i += 2)
+                {
+                    byte[] findBinary = patchPairs[i];
+                    byte[] replaceBinary = patchPairs[i + 1];
+
+                    // Search for the 'findBinary' sequence in the byteArray and replace it with 'replaceBinary'.
+                    for (int j = 0; j <= byteArray.Length - findBinary.Length; j++)
+                    {
+                        if (isSequenceValid(byteArray, j, findBinary))
+                        {
+                            for (int k = 0; k < findBinary.Length; k++)
+                                byteArray[j + k] = replaceBinary[k];
+                            break;
+                        }
+                    }
+                }
             }
-
-            return true;
-
+            catch (Exception e)
+            {
+                LastLog = $"Error: {e.Message}";
+            }
         }
 
-        private bool isSequenceVaild(byte[] seq, int pos)
+        // Method to check if a sequence is valid at a given position in the byte array.
+        private bool isSequenceValid(byte[] byteArray, int startIndex, byte[] sequenceToFind)
         {
-
-            if (pos + FindBinary.Length > seq.Length)
+            // Check if the 'findBinary' sequence can fit within the 'byteArray' starting from 'startIndex'.
+            if (startIndex + sequenceToFind.Length > byteArray.Length)
                 return false;
 
-            for (int i = 0; i <= FindBinary.Length - 1; i++)
-                if (FindBinary[i] != seq[pos + i])
+            // Compare each byte of 'findBinary' with the corresponding byte in 'byteArray'.
+            for (int i = 0; i <= sequenceToFind.Length - 1; i++)
+                if (sequenceToFind[i] != byteArray[startIndex + i])
                     return false;
 
             return true;
         }
 
-        public byte[] getPatchedData()
+        // Method to check if the size of patchPairs is valid (even number of elements).
+        private bool IsPatchPairsSizeValid(byte[][] patchPairs)
         {
-            return ByteArray;
+            if (patchPairs.Length % 2 != 0)
+                throw new ArgumentException($"{LIB_NAME}: The patchPairs must contain an equal number of 'find' and 'replace' patterns, size of array must be even.\nSize of array: {patchPairs.Length}");
+
+            return true;
         }
 
-        public string getlastLog()
+        /// <returns>The patched byte array.</returns>
+        public byte[] GetPatchedBinary()
+        {
+            return byteArray;
+        }
+
+        /// <returns>The last log message.</returns>
+        public string GetLastLog()
         {
             return LastLog;
         }
 
+        // Dispose method to release resources.
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        // Dispose method for releasing resources.
         protected virtual void Dispose(bool disposing)
         {
-            FindBinary = null;
-            ReplaceBinary = null;
+            byteArray = null;
             LastLog = null;
         }
-
     }
 }
--- README.md
+++ README.md
@@ -1,22 +1,86 @@
+# PatchIt - BinaryPatcher
+
+BinaryPatcher is a C# class library designed for patching binary data efficiently. This library allows you to find and replace specific binary sequences within a byte array. It's particularly useful for tasks like modifying binary files, applying patches, or customizing data structures.
+
+## What's New in Version 1.1.0.0
+
+### Major Changes
+
+##### Renamed the Class
+- Although the namespace is the same `PatchIt`. The class name has been changed from `Patch` to `BinaryPatcher` for better clarity and consistency.
+
+##### Library Name Constant
+- Added a constant `LIB_NAME` to store the library name for consistent error messages.
+
+##### Improved Error Handling
+- Includes enhanced error handling. It provides more informative error messages and logs to help users diagnose and address issues when they occur, making it easier to pinpoint issues.
+
+##### Logging & Null Check
+- Added a `LastLog` property to provide information about the last operation's status.
+- Added a null check for the input byte array in the constructor.
+
+##### Code Refactoring
+- The code has been refactored for improved readability and maintainability. Variable names and method names have been updated to be more descriptive, making it easier for developers to understand the code.
+
+##### Additional Comments
+- Includes additional comments and documentation to make the code more self-explanatory for users and developers. This documentation helps users understand how to use the class effectively (although the library is small).
+
+### Usage
+
+#### Using 'using' statement for proper resource disposal
+
+```csharp
+// Example usage of BinaryPatcher using the 'using' statement for proper resource disposal
+using PatchIt;
+
+// Define an array of patch pairs (find-replace) as byte arrays (must be even number of elements)
+byte[][] patchPairs = { findPattern1, replacePattern1, findPattern2, replacePattern2 };
+//
+using (BinaryPatcher patcher = new BinaryPatcher(bytes)) {
+    // Apply the patches
+    patcher.PatchData(patchPairs);
+    // Get the patched byte array if GetLastLog() is equal to 'Success'
+    if (patcher.GetLastLog() == "Success")
+        byte[] patchedData = patcher.GetPatchedBinary();
+}
+```
+
+#### Without Using 'using' statement
+
+```csharp
+// Example usage of BinaryPatcher using the 'using' statement for proper resource disposal
+using PatchIt;
+
+// Initialize BinaryPatcher with a byte array
+var patcher = new BinaryPatcher(byteArray);
+// Define an array of patch pairs (find-replace) as byte arrays (must be even number of elements)
+byte[][] patchPairs = { findPattern1, replacePattern1, findPattern2, replacePattern2 };
+// Apply the patches
+patcher.PatchData(patchPairs);
+// Get the patched byte array if GetLastLog() is equal to 'Success'
+if (patcher.GetLastLog() == "Success")
+    byte[] patchedData = patcher.GetPatchedBinary();
+```
+---
 ### What is CS PatchIt?
 It is a super easy library to edit binary files as to your need for.
 
-### How to use it?
-just simply by **_finding a set of values_** and **_replacing them with another set_**, essentially using **jagged arrays**.
+### How to ...?
+Just simply by **_finding a set of values_** and **_replacing them with another set_**, essentially using **jagged arrays**.
 
-_After referencing the library (PatchIt.dll) into your project then we just create the object._
+_After referencing the library (PatchIt.dll) or copying 'PatchIt.cs' into your project then you just include it in your project by `using PatchIt` then create the object._
 
-_When creating the object **Patch()** we must provide one parameters,_
-1. The binary data, must be stated
+_When creating the object **BinaryPatcher()** we must provide one parameter,_
+- The binary data, must be stated
 
-_When using the method **patchData()** we must provide one parameter,_
-1. The jagged array (array of arrays) always must be an even number of arrays, every odd array is the "Find values" and every even array is the "Replace with values".
+_When using the method **PatchData()** we must provide one parameter,_
+- The jagged array or the array of `pairs`(**find binary array** and **replace binary array**) always must be an even number of arrays in `pairs`, in each `pair` the frist array is the "Find values" and the second  is the "Replace with values".
 
-Finally, use the method _getPatchedData()_ to write the new data to a variable.
+_Finally, use the method **GetPatchedData()** to write the new data to a variable._
 
 ### Simple test code review
 Lets assume we have the following code
-![Screenshot 2021-07-22 074315](https://user-images.githubusercontent.com/19475395/126592184-e920d846-53ea-4fa8-aa31-bc7022b944a5.png)
+![Screenshot 2023-09-13 212616](https://github.com/AhaTheGhost/cs-patchit/assets/19475395/99e54ff2-c785-4b28-baa3-e6aab15d1b59)
 
 
 The above code will result us as the following,
@@ -30,3 +94,8 @@ Furthermore,
 - And finally, the third was also found and replaced.
 
 _The algorithm was writing to make it as simple and as lightweight as possible with as little of **n** of **O(n)**. Also note that it will depend on your file size_
+
+---
+For more information and to access the code, please visit the [GitHub repository](https://github.com/AhaTheGhost/cs-patchit/).
+If you have any questions or encounter any issues, please feel free to [reach out](mailto:ahmad360pro@gmail.com).
+Thank you for using the `PatchIt-BinaryPatcher` library, and hope you find this update beneficial.
